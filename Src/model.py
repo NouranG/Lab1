433c1a4a4d0dd5af66bf5af784bdebf714403891
nouranlahzy38 <nouranlahzy38@gmail.com>
2026-04-21 13:55:10 +0000

    Code formatting

--- Src/model.py
+++ Src/model.py
@@ -1,21 +1,12 @@
 from lightgbm import LGBMClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-def get_model(model_name):
-    if model_name=='lgb':
-        
-        return LGBMClassifier(
-            n_estimators=300,
-            learning_rate=0.05,
-            random_state=42
-        )
-    elif model_name =='rf':
-        return RandomForestClassifier(
-            n_estimators=100,
-            random_state=42,
-            max_depth=5
 
-        )
+def get_model(model_name):
+    if model_name == "lgb":
+
+        return LGBMClassifier(n_estimators=300, learning_rate=0.05, random_state=42)
+    elif model_name == "rf":
+        return RandomForestClassifier(n_estimators=100, random_state=42, max_depth=5)
     else:
         raise ValueError("Unknown model")
-        
--- Src/preprocessing.py
+++ Src/preprocessing.py
@@ -1,31 +1,40 @@
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, OrdinalEncoder
 from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+
 
 class DataPreprocessor:
 
     def drop_columns(self, df):
-        df = df.drop(['PassengerId', 'Name'], axis=1)
+        df = df.drop(["PassengerId", "Name"], axis=1)
         return df
 
     def build_pipeline(self, df):
-        numerical_cols = df.select_dtypes(include=['number']).columns.tolist()
-        categorical_cols = df.select_dtypes(include=['object']).columns.tolist()
+        numerical_cols = df.select_dtypes(include=["number"]).columns.tolist()
+        categorical_cols = df.select_dtypes(include=["object"]).columns.tolist()
 
-        num_pipeline = Pipeline([
-            ('impute', SimpleImputer(strategy='median')),
-            ('scale', StandardScaler())
-        ])
+        num_pipeline = Pipeline(
+            [("impute", SimpleImputer(strategy="median")), ("scale", StandardScaler())]
+        )
 
-        cat_pipeline = Pipeline([
-            ('impute', SimpleImputer(strategy='most_frequent')),
-            ('encode', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1))
-        ])
+        cat_pipeline = Pipeline(
+            [
+                ("impute", SimpleImputer(strategy="most_frequent")),
+                (
+                    "encode",
+                    OrdinalEncoder(
+                        handle_unknown="use_encoded_value", unknown_value=-1
+                    ),
+                ),
+            ]
+        )
 
-        preprocessor = ColumnTransformer([
-            ('num', num_pipeline, numerical_cols),
-            ('cat', cat_pipeline, categorical_cols)
-        ])
+        preprocessor = ColumnTransformer(
+            [
+                ("num", num_pipeline, numerical_cols),
+                ("cat", cat_pipeline, categorical_cols),
+            ]
+        )
 
         return preprocessor
